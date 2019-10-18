6cb36eaa3db813194de60c6abf941c7bb4649654
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-10-18 13:32:05 -0400

    hang up on search, 1st

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,20 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CharacterCard from './CharacterCard';
+import axios from 'axios';
 import './Components.css';
 
 export default function SearchForm() {
- 
+  const [char, setChar] = useState([]);
+  const [search, setSearch] = useState('');
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        const data = res.data.results;
+        const result = data.filter(item =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        );
+        setChar(result);
+      })
+  },[search]);
+  const handleInputChange = (e) => {
+    e.preventDefault()
+    setSearch(e.target.value);
+  }
   return (
     <section className="search-form">
     
-      <form className='formSearch'>
+      <form className='formSearch' onSubmit={handleInputChange}>
       <span className='link1'>Character List</span>
       <div className='search'>
-        <input className=''
+        <input
+        value={search}
+        className=''
         type='text'
         placeholder='Search' />
-        <button className=''>Go</button>
+        <button className='' type='submit'>Go</button>
         </div>
       </form>
+      {char.map((character, index) => {
+        return (<CharacterCard key={index} {...character} />)
+      })}
     </section>
   );
 }
